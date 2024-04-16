a7225a1fc015c9991dc380660ba20c82acb443a2
ADNAN ADIL <adnanadil@ADNANs-MacBook-Pro.local>
2024-04-17 00:24:57 +0300

    cors updated

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,9 @@ const planetsRoute = require('./routes/planets/planets.router')
 
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin: "*"
+}))
 app.use(express.json())
 app.use(planetsRoute)
 
